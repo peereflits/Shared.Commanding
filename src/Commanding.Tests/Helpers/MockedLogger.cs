c78829f2f9e32549aa5862b11ef07f9bc12c5902
peereflits <peereflits@live.com>
2023-09-06 16:33:37 +0200

    Add ci/cd, multi targeting 6.0/7.0, shared build props

--- src/Commanding.Tests/Helpers/MockedLogger.cs
+++ src/Commanding.Tests/Helpers/MockedLogger.cs
@@ -5,12 +5,12 @@ namespace Peereflits.Shared.Commanding.Tests.Helpers;
 
 public abstract class MockedLogger<T> : ILogger<T>
 {
-    void ILogger.Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter) =>
+    void ILogger.Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter) =>
             Log(logLevel, formatter(state, exception));
 
     public abstract void Log(LogLevel logLevel, string message);
 
     public virtual bool IsEnabled(LogLevel logLevel) => true;
 
-    public abstract IDisposable BeginScope<TState>(TState state);
+    public abstract IDisposable BeginScope<TState>(TState state) where TState : notnull;
 }
